978409938d6aac08087a1237123440046545d05d
DAVIDMA0808 <jianxiang.ma@mail.utoronto.ca>
2024-07-17 15:21:56 -0400

    Changed some filenames.

---
+++ src/app/firebase/config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCQ0QTks2_rcej3Lw4dUDexTwm2D5t_VYY",
+    authDomain: "fiscord-cb307.firebaseapp.com",
+    databaseURL: "https://fiscord-cb307-default-rtdb.firebaseio.com",
+    projectId: "fiscord-cb307",
+    storageBucket: "fiscord-cb307.appspot.com",
+    messagingSenderId: "327891496432",
+    appId: "1:327891496432:web:a2297009f82d483478c135"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
